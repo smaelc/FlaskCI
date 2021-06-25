1041e006a34d78967bf47ea0bf5ce68eecca3756
Smael CHECK <smael.m.check@gmail.com>
2021-06-25 20:25:16 +0200

    Correction de parametres et autres

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 from flask import render_template
-from .settings import app
+from settings import app
 
 @app.route('/', methods=('GET',))
 def index():
--- settings.py
+++ settings.py
@@ -4,12 +4,13 @@ import os
 from flask import Flask
 
 BASE_DIR        = os.path.dirname(__name__)
-TEMPLATES_DIR   = os.path.join(BASE_DIR, 'templates')
-STATIC_DIR      = os.path.join(BASE_DIR, 'static')
+TEMPLATES_DIR   = os.path.join(BASE_DIR, './templates')
+STATIC_DIR      = os.path.join(BASE_DIR, './static')
 
 app             = Flask(__name__,
-                        static_url_path=STATIC_DIR,
+                        static_folder=STATIC_DIR,
                         template_folder=TEMPLATES_DIR
                 )
 
+app.config['SECRET_KEY'] = 'circleci-test-ci/cd'
 
